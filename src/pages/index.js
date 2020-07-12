d86eae18d0f2d0675044e8c12c16f30b4fb9b84e
Elliot Newnham <elliot.newnham@gmail.com>
2020-07-12 13:58:22 -0400

    Add a meme about old sites

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ export default function Home() {
         <h1>Hey there and welcome to my attempt at social media 👋</h1>
 
         <li>
-            <ul>📫 E-Mail me at `elliot.newnham@gmail.com` or we can talk in git issues I guess?</ul>
+            <ul>📫 <a href="mailto: elliot.newnham@gmail.com">elliot.newnham@gmail.com</a></ul>
+            <ul>Are those "under construction" placeholders from the 90's still a thing?</ul>
+            <ul>CSS is overrated</ul>
         </li>
     </>
 }
