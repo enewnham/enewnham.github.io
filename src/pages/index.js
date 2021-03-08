60507f7d307ed75854504b28c72dc4866d6113da
Elliot Newnham <enewnham@mri-inc.net>
2021-03-08 12:57:48 -0500

    Add Orleans.Providers.MSSQLDapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,5 +8,10 @@ export default function Home() {
             <ul>Are those "under construction" placeholders from the 90's still a thing?</ul>
             <ul>CSS is overrated</ul>
         </li>
+        
+        <h3><a href="https://github.com/enewnham/Orleans.Providers.MSSQLDapper">Orleans.Providers.MSSQLDapper</a></h3>
+        <p>
+            Optimized Orleans providers for Microsoft SQL Server for In-Memory OLTP and Natively Compiled Stored Procedures
+        </p>
     </>
 }
