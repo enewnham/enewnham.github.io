2255f245ee2bf9e2e4cc9c73062792ce081dd327
Elliot Newnham <elliot.newnham@gmail.com>
2021-06-09 17:54:17 -0400

    Add a recursive call to this portfolio to the portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,5 +17,15 @@ export default () => (
       Optimized Orleans providers for Microsoft SQL Server for In-Memory OLTP
       and Natively Compiled Stored Procedures
     </p>
+
+    <TitleAnchor href="https://github.com/enewnham/enewnham.github.io">
+      enewnham.github.io
+    </TitleAnchor>
+    <p>
+      A gatsby JS resume written using best practices in react, styled
+      components, and typescript. While I'm not a full-time front-end engineer I
+      have a solid foundation knowledge in modern techniques in building
+      websites.
+    </p>
   </Layout>
 );
